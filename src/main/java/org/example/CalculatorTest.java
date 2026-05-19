d8cd544f8fc515f03b4cd5f950a1a92f1bd335e2
qcios <2797698717@qq.com>
2026-05-19 22:55:09 +0800

    Add files via upload

---
+++ src/main/java/org/example/CalculatorTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    public void testSub() {
+        assertEquals(3, calc.sub(5, 2));
+    }
+
+    @Test
+    public void testMul() {
+        assertEquals(12, calc.mul(3, 4));
+    }
+
+    @Test
+    public void testDiv() {
+        assertEquals(5.0, calc.div(10, 2), 0.001);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivZero() {
+        calc.div(5, 0);
+    }
+}
